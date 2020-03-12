7b648ba7db6803df03751e59652685cb129db209
sbullard <censortrip@gmail.com>
2020-03-12 13:16:19 -0400

    Update commands.py

--- commands.py
+++ commands.py
@@ -2,13 +2,14 @@ import click
 from flask.cli import with_appcontext
 from models import *
 
+#note db variable is created in models
 def loadCSV():
 	f = open("books.csv")
 	reader = csv.reader(f)
 	for isbn, title, author, year in reader:
-		testdb.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title , :author ,:year)", {"isbn":isbn, "title":title, "author":author, "year":year})
+		db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title , :author ,:year)", {"isbn":isbn, "title":title, "author":author, "year":year})
 		print(f"Added book to db: title:{title}, author:{author}, year:{year}, isbn:{isbn}")
-	testdb.commit()
+	db.commit()
 	
 # This is used for deployement on heroku specifically.
 # it allows for database table creation and to populate the books table
